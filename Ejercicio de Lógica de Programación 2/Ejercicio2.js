0fd28ec06dde048db6a44996f543cc10f08eff7f
NatLop <nplopez_s@hotmail.com>
2023-03-29 20:33:37 -0600

    Correccion Ejercicio 2

--- Ejercicio de Lógica de Programación 2/Ejercicio2.js
+++ Ejercicio de Lógica de Programación 2/Ejercicio2.js
@@ -1,9 +1,18 @@
 var readlineSync = require('readline-sync');
-var numero1 = parseInt(readlineSync.question('Dame el numero 1: '));
-var numero2 = parseInt(readlineSync.question('Dame el numero 2: '));
-var numero3 = parseInt(readlineSync.question('Dame el numero 3: '));
+var temperatura = parseInt(readlineSync.question('Dame la temperarura en Celcius: '));
 
-var array = [numero1,numero2,numero3];
-console.log(array.sort());
-console.log(array.reverse())
+//Funcion de Celcius a Farenheit
+function changeToFarenheit(temperatura){
+    let farenheit = (1.8*temperatura)+32;
+    console.log("Grados Fahrenheit:", farenheit)
+}
+
+//Funcion de Celcius a Kelvin
+function changeToKelvin(temperatura){
+    let kelvin = temperatura + 273.15;
+    console.log("Grados Kelvin:", kelvin);
+}
+
+changeToFarenheit(temperatura);
+changeToKelvin(temperatura);
 
